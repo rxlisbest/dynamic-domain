3308cfb9a70f84c7188e70cc000730307cc86d70
Roy <rxlisbest@gmail.com>
2018-08-23 17:34:35 +0800

    增加redis缓存map

---
+++ config/redis.lua
@@ -0,0 +1,6 @@
+return {
+            host = "127.0.0.1",
+            port = 6379,
+            user = "root",
+            password = "root",
+        }
--- proxy.lua
+++ proxy.lua
@@ -1,31 +1,43 @@
--- ngx.exec('/1/bucket_1' .. '/' ..ngx.var.uri)
--- ngx.say(ngx.var.uri)
--- ngx.say(ngx.var.host)
-
-local mysql = require "lib.resty.mysql"
-local mysql_config = require "config.mysql"
-
-local db, err = mysql:new()
-if not db then
-    ngx.say("failed to instantiate mysql: ", err)
-    return
-end
-local ok, err, errcode, sqlstate = db:connect(mysql_config)
+local redis = require "lib.resty.redis"
+local red = redis:new()
+local redis_config = require "config.redis"
+local ok, err = red:connect(redis_config.host, redis_config.port)
 if not ok then
-    ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
+    ngx.say("failed to connect: ", err)
     return
 end
-local sql = "SELECT * FROM map WHERE domain = '" .. ngx.var.host .. "'"
-ngx.say(sql)
-local res, err, errno, sqlstate = db:query(sql)
-db:close()
--- ngx.say(#res)
-if not res then
-    ngx.say(err)
+
+local res, err = red:get(ngx.var.host)
+if res == ngx.null then
+    local mysql = require "lib.resty.mysql"
+    local mysql_config = require "config.mysql"
+
+    local db, err = mysql:new()
+    if not db then
+        ngx.say("failed to instantiate mysql: ", err)
+        return
+    end
+    local ok, err, errcode, sqlstate = db:connect(mysql_config)
+    if not ok then
+        ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
+        return
+    end
+    local sql = "SELECT * FROM map WHERE domain = '" .. ngx.var.host .. "'"
+
+    local res, err, errno, sqlstate = db:query(sql)
+    db:close()
+    -- ngx.say(#res)
+    if not res then
+        ngx.say(err)
+        return
+    end
+    if #res > 0 then
+        -- ngx.say(res[1]['directory'] .. ngx.var.uri)
+        ok, err = red:set(ngx.var.host, res[1]['directory'])
+        ngx.exec(res[1]['directory'] .. string.sub(ngx.var.uri, 2))
+    end
     return
+else
+    ngx.exec(res .. string.sub(ngx.var.uri, 2))
 end
-    ngx.say(res[1]['directory'] .. ngx.var.uri)
-if #res > 0 then
-    ngx.say(res[1]['directory'] .. ngx.var.uri)
-    -- ngx.exec(res[1]['directory'] .. ngx.var.uri)
-end
+
