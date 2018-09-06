6226393989ee488aa1ccbef2d1c2abf638694dc1
Roy <rxlisbest@gmail.com>
2018-09-06 21:14:02 +0800

    修改接口

--- api.lua
+++ api.lua
@@ -1,43 +1,57 @@
 package.cpath = ngx.var.lua_dir .. "lib/?.so;;"
 package.path = ngx.var.lua_dir .. "?.lua;;"
 local cjson = require "cjson"
-if true then
-    local test = {}
-    test['a'] = 1
-    ngx.say(cjson.encode(test))
-    return
+
+function e(message)
+    local r = {}
+    r['code'] = 0 
+    r['message'] = message
+    ngx.say(cjson.encode(r))
+end
+function s(message)
+    local r = {}
+    r['code'] = 1 
+    r['message'] = message
+    ngx.say(cjson.encode(r))
+end
+
+if "POST" ~= ngx.var.request_method then
+    return e('Only accept POST request.')
+end
+
+local header = ngx.req.get_headers()
+if "application/x-www-form-urlencoded" ~= header['content-type'] then
+    return e('Only accept Content-Type:application/x-www-form-urlencoded.')
 end
 
 ngx.req.read_body()
-local postargs = ngx.req.get_post_args()
-ngx.say(postargs["test"])
+local post = ngx.req.get_post_args()
+if post['domain'] == nil then
+    return e('Parameter domain can not empty.')
+end
+if post['directory'] == nil then
+    return e('Parameter directory can not empty.')
+end
+
 local mysql = require "lib.resty.mysql"
 local mysql_config = require "config.mysql"
 
 local db, err = mysql:new()
 if not db then
-    ngx.say("failed to instantiate mysql: ", err)
-    return
+    return e("failed to instantiate mysql: ", err)
 end
 local ok, err, errcode, sqlstate = db:connect(mysql_config)
 if not ok then
-    ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
-    return
+    return e("failed to connect: ", err, ": ", errcode, " ", sqlstate)
 end
-local sql = "SELECT * FROM map WHERE domain = '" .. ngx.var.host .. "'"
+local sql = string.format( "INSERT INTO map (domain, directory) VALUES ('%s', '%s')", post['domain'], post['directory'])
 
 local res, err, errno, sqlstate = db:query(sql)
 db:close()
 -- ngx.say(#res)
 if not res then
-    ngx.say(err)
-    return
+    return e(err)
 end
-if #res > 0 then
-    ok, err = red:set(redis_config.prefix .. ngx.var.host, res[1]['directory'])
-end
-
-
 local redis = require "lib.resty.redis"
 local red = redis:new()
 local redis_config = require "config.redis"
@@ -46,15 +60,5 @@ if not ok then
     ngx.say("failed to connect: ", err)
     return
 end
-
-local res, err = red:get(redis_config.prefix .. ngx.var.host)
-if not res then
-    ngx.say("failed to get the key: ", err)
-    return
-end
-if res == ngx.null then
-    return
-else
-    return 
-end
-
+ok, err = red:set(redis_config.prefix .. post['domain'], post['directory'])
+return s('ok')
--- config/mysql.lua
+++ config/mysql.lua
@@ -1,10 +1,10 @@
 return {
             host = "127.0.0.1",
             port = 3306,
-            database = "upload_server",
+            database = "dynamic_domain",
             user = "root",
             password = "root",
             charset = "utf8",
-            prefix = "rrs_",
+            -- prefix = "rrs_",
             max_packet_size = 1024 * 1024,
         }
